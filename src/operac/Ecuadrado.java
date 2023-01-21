cb8f22807d1578514c44e983bd2c28d2c17b2330
mariasilvah <122307945+mariasilvah@users.noreply.github.com>
2023-01-21 10:39:15 -0600

    Add files via upload

---
+++ src/operac/Ecuadrado.java
@@ -0,0 +1,22 @@
+package operac;
+public class Ecuadrado {
+	public double l;
+	public Ecuadrado() {
+		l=160808;
+	}
+	public double calcDiago()
+	{
+		return Math.sqrt(Math.pow(l, 2)+ Math.pow(l, 2));
+	}
+	public double calcArea()
+	{
+		return Math.pow(l, 2);
+	}
+	public double calcPeri()
+	{
+		return l*4;
+	}
+	public void ladoDife(double d) {
+		l = d;
+	}
+}
---
+++ src/operac/Niam.java
@@ -0,0 +1,11 @@
+package operac;
+public class Niam {
+	public static void main (String[]args) {
+			
+			Ecuadrado a = new Ecuadrado();
+			System.out.println(a.calcDiago());
+			System.out.println(a.calcArea());
+			a.ladoDife(153139);
+			System.out.println(a.calcPeri());
+	}
+}
